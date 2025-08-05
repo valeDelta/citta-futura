b50a4ec209492a313782c3248719c519eadf924b
valeDelta <valerio.evarchi@deltainformatica.eu>
2025-08-05 10:13:04 +0200

    sistemato ordine di esecuzione comandi e aggiornato xml e setup office

--- 4_config.ps1
+++ 4_config.ps1
@@ -1,5 +1,7 @@
 Unregister-ScheduledTask -TaskName "continue" -Confirm:$False
 
+$PD = "C:\Users\Public\Desktop"
+
 #non funziona se non sul desktop
 Set-WinHomeLocation -GeoId 118
 
@@ -7,10 +9,6 @@ Set-WinHomeLocation -GeoId 118
 net start w32time
 w32tm /resync
 
-changepk.exe /ProductKey NWHDX-K9VRX-KRHRM-VMTK2-8XDMX
-$key = Read-Host "
-Inserire la nuova key di windows"
-changepk.exe /ProductKey $key
 
 Get-AppxPackage -AllUsers "MSTeams" | Remove-AppxPackage -AllUsers
 Get-AppxPackage -AllUsers "McAfeeWPSSparsePackage" | Remove-AppxPackage -AllUsers
@@ -26,23 +24,26 @@ Get-AppxPackage -AllUsers "Microsoft.XboxGameCallableUI" | Remove-AppxPackage -A
 Get-AppPackage -AllUsers "Microsoft.GamingApp" | Remove-AppPackage -AllUsers
 Get-AppPackage -AllUsers "Microsoft.OutlookForWindows" | Remove-AppPackage -AllUsers
 
+#disinstall mcafee
 Set-Location "C:\Program Files\McAfee\WebAdvisor"
 .\uninstaller.exe /s
-
-
+Set-Location "C:\Program Files\McAfee"
 Set-Location "C:\Program Files\McAfee\wps\1.31.148.1"
 .\mc-update.exe -uninstall -silent -force
 
-
+#install office
 Set-Location "C:\temp\"
-.\setup.exe /dowload "C:\temp\conf.xml"
-.\setup.exe /configure "C:\temp\conf.xml"
+.\setup.exe /dowload "C:\temp\office.xml"
+.\setup.exe /configure "C:\temp\office.xml"
 
+$PC = Read-Host "
+Inserire il nuovo nominativo per il computer
+"
 
 $client = Read-Host "
 Computer da:
-1: sede
-2: asili
+1: asili
+2: sede
 3: nessuno
 "
 if ((Get-NetConnectionProfile).IPv4Connectivity -contains "Internet" -or (Get-NetConnectionProfile).IPv6Connectivity -contains "Internet") { 
@@ -52,20 +53,32 @@ if ((Get-NetConnectionProfile).IPv4Connectivity -contains "Internet" -or (Get-Ne
  Switch ($client)
 {
     "1"  { 
-        Invoke-WebRequest '' -OutFile "$PD\teleassistenza.exe"
+        Invoke-WebRequest 'https://redirector.eset.systems/li-handler/?uuid=epi_win-dea2c64c-ca8b-4bf6-9449-cda56cb83add' -OutFile "C:\temp\eset.exe"
+        Set-Location "C:\temp\"
+        eset.exe
      }
     "2"{ 
-       Invoke-WebRequest '' -OutFile "$PD\teleassistenza.exe"
+       Invoke-WebRequest 'https://redirector.eset.systems/li-handler/?uuid=epi_win-b208bcc5-97da-424f-b62f-ee836e00c809' -OutFile "C:\temp\eset.exe"
+       Set-Location "C:\temp\"
+        eset.exe
      }
      "3"{
-        exit
+        continue
      }
 }
 
-$PC = Read-Host "
-Inserire il nuovo nominativo per il computer
-"
 Rename-Computer -NewName $PC
+
+$key = Read-Host "
+Inserire la nuova key di windows"
+try {
+    changepk.exe /ProductKey $key
+}
+catch {
+    changepk.exe /ProductKey NWHDX-K9VRX-KRHRM-VMTK2-8XDMX
+    changepk.exe /ProductKey $key
+}
+
 
 pause
 
