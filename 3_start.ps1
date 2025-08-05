bbb988a1736f4680f998bcdb1aca94dcbc9f88ea
valeDelta <valerio.evarchi@deltainformatica.eu>
2025-08-05 09:01:16 +0200

    corretta la condizione di attivazione dell'attività programmata

--- 3_start.ps1
+++ 3_start.ps1
@@ -2,14 +2,13 @@
     # creo le azioni da eseguire
     Set-ExecutionPolicy Unrestricted -Scope LocalMachine
     $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "C:\temp\config.ps1 -WindowStyle Maximized"
-    #$action1 = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "C:\temp\update.ps1 -WindowStyle Hidden"
     #creo il trigger e impostazioni per l'attività
     $trigger = New-ScheduledTaskTrigger -AtLogOn
-    $task = New-ScheduledTaskPrincipal -RunLevel Highest -UserId Utente
+    $task = New-ScheduledTaskPrincipal -RunLevel Highest -UserId Saidea
 
     # registro le attività
     Register-ScheduledTask -Action $action -Trigger $trigger -Principal $task -TaskName "continue"
-    #Register-ScheduledTask -Action $action1 -Trigger $trigger -Principal $task -TaskName "update"
+
 } 
 else { 
     exit
